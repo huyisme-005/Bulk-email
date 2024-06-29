9f68167c8dd2a71d51a4109d122273950dbdfa70
huyisme-005 <legiahuy050104@gmail.com>
2024-06-29 16:53:38 -0400

    New change

--- antd-demo/src/script.js
+++ antd-demo/src/script.js
@@ -3,6 +3,6 @@ import user from "./sample.json" assert { type: 'json' };
 console.log(user)
 const content=require("./sample.json");
 let data=JSON.parse(content);
-data.forEach(item => {
-    item.email=
+data.forEach((item) => {
+    item['Email']=[];
 });
