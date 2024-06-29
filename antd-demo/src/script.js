269edeefcdeaaebdf4065ebd4b0ef2b611f7b275
huyisme-005 <legiahuy050104@gmail.com>
2024-06-29 17:10:45 -0400

    New change

--- antd-demo/src/script.js
+++ antd-demo/src/script.js
@@ -2,7 +2,7 @@
 //import user from "./sample.json" assert { type: 'json' };
 //console.log(user)
 const content=require("./sample.json");
-var data=JSON.parse(content);
+var data=JSON.parse(JSON.stringify(content));
 data.forEach((item) => {
     item["Email aliases"]=[item["First name"]+item["Address"].split(", ").slice(0,2)
 +"@platihub.com",item["Last name"]+item["Birthplace"].split(", ").slice(0,2)+"@platihub.com"
