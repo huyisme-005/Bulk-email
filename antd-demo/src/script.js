363ff9624220ec545382f71249926d36930d2ce2
huyisme-005 <legiahuy050104@gmail.com>
2024-06-29 16:32:57 -0400

    New change

--- antd-demo/src/script.js
+++ antd-demo/src/script.js
@@ -1,5 +1,5 @@
 // script.js
 import user from "./sample.json" assert { type: 'json' };
 console.log(user)
-const content=require("./sample.json")
+const content=require("./sample.json");
 const data=JSON.parse(content);
