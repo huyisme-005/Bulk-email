711bbd83cf31a75a4fdfdca370e945cabf246244
huyisme-005 <legiahuy050104@gmail.com>
2024-06-29 17:06:48 -0400

    New change

--- antd-demo/src/script.js
+++ antd-demo/src/script.js
@@ -5,5 +5,7 @@ const content=require("./sample.json");
 var data=JSON.parse(content);
 data.forEach((item) => {
     item["Email aliases"]=[item["First name"]+item["Address"].split(", ").slice(0,2)
-+"@platihub.com",item["Last name"]+item["Birthplace"]];
++"@platihub.com",item["Last name"]+item["Birthplace"].split(", ").slice(0,2)+"@platihub.com"
+,item["Birthplace"].split(", ")[2].split(" ").join("_")+item["Phone number"].split("-")[1]
++"@platihub.com"];
 });
