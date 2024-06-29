dc0e2527f6de1c7de9dad632f36def4a66e7391c
huyisme-005 <legiahuy050104@gmail.com>
2024-06-29 15:16:49 -0400

    New change

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -4,43 +4,14 @@ import {Button} from 'antd';
 import defaultExport  from './table.js'
 import Table from './table.js';
 import {React} from 'react';
+import ExcelToJsonConverter from './ExcelToJsonConverter';
 
-
-async function App() {
-  const columns = [
-    {
-      name:"Name",
-      selector:row=>row.name
-    },
-    {
-      name:"Email",
-      selector:row=>row.email
-    },
-    {
-      name:"Phone number",
-      selector:row=>row.phone_number
-    },
-    
-  ];
-const url="https://docs.sheetjs.com/executive.json";
-
-const data = await (await fetch(url)).json();
-
-
-
- 
- 
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <Button type='primary'>My First Button</Button>
-        
-        <Table columns={columns} dataSource={data} />
-      </header>
-    </div>
+      <div className="App">
+          <ExcelToJsonConverter />
+      </div>
   );
 }
 
 export default App;
-
