6bec42bb123c54a62c8b051c19d3af74359833d2
huyisme-005 <legiahuy050104@gmail.com>
2024-06-29 23:21:31 -0400

    New change

--- antd-demo/src/script.js
+++ antd-demo/src/script.js
@@ -4,7 +4,7 @@
 //const filename=prompt("Input a json filename:");
 "use client";
 import {useEffect} from "react";
-import content from "./sample.json" assert {type:"json"};
+import content from "./sample.json" with {type:"json"};
 //const content=require(filename);
 var data=JSON.parse(JSON.stringify(content));
 data.forEach((item) => {
